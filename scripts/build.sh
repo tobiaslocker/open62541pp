a75389732bf91150392ad096397b9e9f7b7533a8
tobiaslocker <tobias@tobiaslocker.de>
2019-05-13 20:57:26 +0100

    Update gitignore fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,10 @@ export open62541_DIR=${HOME}/Projects/open62541-cpp-wrapper/externals/lib/cmake
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 TOP_DIR=${SCRIPTS_DIR}/..
 
-mkdir ${TOP_DIR}/externals/open62541/build/
-cd ${TOP_DIR}/externals/open62541/build/
+mkdir ${TOP_DIR}/externals/build/
+cd ${TOP_DIR}/externals/build/
 
-cmake -DCMAKE_INSTALL_PREFIX=${TOP_DIR}/externals/ -DUA_ENABLE_AMALGAMATION=true -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUA_ENABLE_FULL_NS0=ON ..
+cmake -DCMAKE_INSTALL_PREFIX=${TOP_DIR}/externals/ -DUA_ENABLE_AMALGAMATION=true -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUA_ENABLE_FULL_NS0=ON ../open62541
 
 make 
 make install
