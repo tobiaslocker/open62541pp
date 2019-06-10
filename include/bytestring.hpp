5aa717733959851732e2ba1d3164b84db32bb6ca
tobiaslocker <tobias@tobiaslocker.de>
2019-06-10 10:16:19 +0100

    Implement DiagnosticInfo

--- include/bytestring.hpp
+++ include/bytestring.hpp
@@ -8,27 +8,6 @@
 namespace open62541 {
 
 using ByteString = std::string;
-// class ByteString {
-//  class impl;
-//  std::unique_ptr<impl> d_ptr;
-
-// public:
-//  ByteString();
-//  ~ByteString();
-
-//  ByteString(ByteString &&) noexcept;
-//  ByteString &operator=(ByteString &&) noexcept;
-//  ByteString(ByteString const &);
-//  ByteString &operator=(ByteString const &);
-
-//  ByteString(std::string const &data);
-
-//  std::string str() const;
-
-//  bool operator==(ByteString const &rhs) const {return true;}
-//  bool operator!=(ByteString const &rhs) const {return false;}
-//;
-//};
 
 }  // namespace open62541
 
--- include/diagnosticinfo.hpp
+++ include/diagnosticinfo.hpp
@@ -1,12 +1,59 @@
 #ifndef OPEN6541_CPP_WRAPPER_DIAGNOSTIC_INFO_H
 #define OPEN6541_CPP_WRAPPER_DIAGNOSTIC_INFO_H
 
+// STL
+#include <memory>
+#include <string>
+
+#include "enums.hpp"
+
 namespace open62541 {
 
 class DiagnosticInfo {
+  class impl;
+  std::unique_ptr<impl> d_ptr;
+
  public:
-  bool operator==(DiagnosticInfo const &rhs) const { return true; }
-  bool operator!=(DiagnosticInfo const &rhs) const { return false; }
+  DiagnosticInfo();
+  ~DiagnosticInfo();
+
+  DiagnosticInfo(DiagnosticInfo &&) noexcept;
+  DiagnosticInfo &operator=(DiagnosticInfo &&) noexcept;
+  DiagnosticInfo(DiagnosticInfo const &);
+  DiagnosticInfo &operator=(DiagnosticInfo const &);
+
+  DiagnosticInfo(bool has_symbolic_id,
+                 bool has_namespace_uri,
+                 bool has_localized_text,
+                 bool has_locale,
+                 bool has_additional_info,
+                 bool has_inner_status_code,
+                 bool has_inner_diagnostic_info,
+                 uint32_t symbolic_id,
+                 uint32_t namespace_uri,
+                 uint32_t localized_text,
+                 uint32_t locale,
+                 std::string additional_info,
+                 StatusCode inner_status_code);
+
+  bool has_symbolic_id() const;
+  bool has_namespace_uri() const;
+  bool has_localized_text() const;
+  bool has_locale() const;
+  bool has_additional_info() const;
+  bool has_inner_status_code() const;
+  bool has_inner_diagnostic_info() const;
+  uint32_t symbolic_id() const;
+  uint32_t namespace_uri() const;
+  uint32_t localized_text() const;
+  uint32_t locale() const;
+  std::string additional_info() const;
+  StatusCode inner_status_code() const;
+  //    class DiagnosticInfo *innerDiagnosticInfo;
+
+ public:
+  bool operator==(DiagnosticInfo const &rhs) const;
+  bool operator!=(DiagnosticInfo const &rhs) const;
 };
 
 }  // namespace open62541
