8a9e7886227eb939d80d5d2b4830e661da21502a
tobiaslocker <tobias@tobiaslocker.de>
2019-06-26 20:34:12 +0100

    Implement responseheader

--- include/responseheader.hpp
+++ include/responseheader.hpp
@@ -1,9 +1,26 @@
 #ifndef OPEN6541_CPP_WRAPPER_RESPONSE_HEADER_H
 #define OPEN6541_CPP_WRAPPER_RESPONSE_HEADER_H
 
+// STL
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "datetime.hpp"
+#include "diagnosticinfo.hpp"
+#include "enums.hpp"
+#include "extensionobject.hpp"
+
 namespace open62541 {
 
 class ResponseHeader {
+  DateTime timestamp;
+  uint32_t requestHandle;
+  StatusCode serviceResult;
+  DiagnosticInfo serviceDiagnostics;
+  std::vector<std::string> stringTable;
+  ExtensionObject additionalHeader;
+
  public:
   bool operator==(ResponseHeader const &rhs) const { return true; }
   bool operator!=(ResponseHeader const &rhs) const { return false; }
