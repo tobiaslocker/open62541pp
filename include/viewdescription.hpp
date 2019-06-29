15c925c796f42b79aebf9645858e58325379c6f5
tobiaslocker <tobias@tobiaslocker.de>
2019-06-29 07:19:14 +0100

    Implement viewdescription

--- include/viewdescription.hpp
+++ include/viewdescription.hpp
@@ -1,12 +1,33 @@
 #ifndef OPEN6541_CPP_WRAPPER_VIEW_DESCRIPTION_H
 #define OPEN6541_CPP_WRAPPER_VIEW_DESCRIPTION_H
 
+// STL
+#include <memory>
+
+#include "nodeid.hpp"
+#include "datetime.hpp"
+
 namespace open62541 {
 
 class ViewDescription {
+  class impl;
+  std::unique_ptr<impl> d_ptr;
+
  public:
-  bool operator==(ViewDescription const &rhs) const { return true; }
-  bool operator!=(ViewDescription const &rhs) const { return false; }
+  ViewDescription();
+  ~ViewDescription();
+
+  ViewDescription(ViewDescription &&) noexcept;
+  ViewDescription &operator=(ViewDescription &&) noexcept;
+  ViewDescription(ViewDescription const &);
+  ViewDescription &operator=(ViewDescription const &);
+
+  ViewDescription(NodeId const &view_id,
+                  DateTime const &timestamp,
+                  uint32_t view_version);
+
+  bool operator==(ViewDescription const &rhs) const;
+  bool operator!=(ViewDescription const &rhs) const;
 };
 
 }  // namespace open62541
