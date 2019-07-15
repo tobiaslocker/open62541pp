ebf9527f518abed6db6a03067289da54b24d03b5
tobiaslocker <tobias@tobiaslocker.de>
2019-07-15 18:56:08 +0100

    Start implement d-pointer for DateTime

--- include/datetime.hpp
+++ include/datetime.hpp
@@ -2,25 +2,25 @@
 #define OPEN6541_CPP_WRAPPER_DATE_TIME_H
 
 // STL
+#include <memory>
 #include <string>
 
 namespace open62541 {
 
 class DateTime {
-  uint16_t m_nano_sec;
-  uint16_t m_micro_sec;
-  uint16_t m_milli_sec;
-  uint16_t m_sec;
-  uint16_t m_min;
-  uint16_t m_hour;
-  uint16_t m_day;
-  uint16_t m_month;
-  uint16_t m_year;
-  int64_t m_number_of_100_nanoseconds_since_jan_1_1601;
+  class impl;
+  std::unique_ptr<impl> d_ptr;
 
  public:
-  DateTime(int64_t number_of_100_nanoseconds_since_jan_1_1601);
+  DateTime();
+  ~DateTime();
 
+  DateTime(DateTime &&) noexcept;
+  DateTime &operator=(DateTime &&) noexcept;
+  DateTime(DateTime const &);
+  DateTime &operator=(DateTime const &);
+
+  DateTime(int64_t number_of_100_nanoseconds_since_jan_1_1601);
   DateTime(uint16_t nano_sec,
            uint16_t micro_sec,
            uint16_t milli_sec,
@@ -29,8 +29,7 @@ class DateTime {
            uint16_t hour,
            uint16_t day,
            uint16_t month,
-           uint16_t year,
-           int64_t number_of_100_nanoseconds_since_jan_1_1601);
+           uint16_t year);
 
   uint16_t nano_sec() const;
   uint16_t micro_sec() const;
@@ -43,8 +42,8 @@ class DateTime {
   uint16_t year() const;
   int64_t number_of_100_nanoseconds_since_jan_1_1601() const;
 
-  bool operator==(DateTime const &rhs) const { return true; }
-  bool operator!=(DateTime const &rhs) const { return false; }
+  bool operator==(DateTime const &rhs) const;
+  bool operator!=(DateTime const &rhs) const;
 };
 
 }  // namespace open62541
