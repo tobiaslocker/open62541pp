e25e595ede6da978694c354cbaec410155add774
tobiaslocker <tobias@tobiaslocker.de>
2019-07-17 09:30:03 +0100

    Clean up example client

--- examples/simple-client/main.cpp
+++ examples/simple-client/main.cpp
@@ -1,8 +1,6 @@
 #include "client.hpp"
 #include <iostream>
 
-#include "open62541.h"
-
 using namespace open62541;
    
 int main(int argc, char *argv[]) {
@@ -11,17 +9,4 @@ int main(int argc, char *argv[]) {
 
     Client client;
     client.connect(url);
-
-    auto root_node = NodeId(0, Identifier(UA_NS0ID_ROOTFOLDER), IdentifierType::Numeric);
-    //auto node = client->node(root_node);
-
-    // Get json representation of root node's nodeid
-    //auto json = node->node_id().to_json();
-    //std::cerr << json << '\n';
-
-    //auto endpoints = client.get_endpoints(url);
-    //std::for_each(
-        //endpoints.begin(), endpoints.end(), [&](EndpointDescription const &endpoint) {
-            //std::cerr << endpoint.endpoint_url() << '\n';
-        //});
 }
