78d9d6fd1a0da3b2ece3de76b66d3ec4810d7bc0
tobiaslocker <tobias@tobiaslocker.de>
2019-07-22 12:41:27 +0100

    Update example

--- examples/simple-client/main.cpp
+++ examples/simple-client/main.cpp
@@ -6,15 +6,15 @@ using namespace open62541;
 
 struct EventHandler : public ClientEventHandler {
   void on_state_changed(ClientState state) override {
-    std::cout << "Hello from Handler!\n";
+    std::cout << "Client state changed to " << state << '\n';
   }
 
   void on_subscription_inactivity(u_int32_t id) override {
-    std::cout << "Hello from Handler!\n";
+    std::cout << "Subscription inactive, id: " << id  << '\n';
   }
 
   void on_inactivity(ClientState state) override {
-    std::cout << "Hello from Handler!\n";
+    std::cout << "Client inactive, state: " << state << '\n';
   }
 };
 
