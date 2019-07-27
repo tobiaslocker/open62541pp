34e8678b474f448826b434d17b9a07a638492cf4
tobiaslocker <tobias@tobiaslocker.de>
2019-07-27 08:55:36 +0100

    WIP: Fix open62541 build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,46 @@
+
 #!/bin/bash
-export open62541_DIR=${HOME}/Projects/open62541-cpp-wrapper/externals/lib/cmake
+#export open62541_DIR=${HOME}/Projects/open62541-cpp-wrapper/third-party/lib/cmake
+
+#SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#TOP_DIR=${SCRIPTS_DIR}/..
+
+#mkdir ${TOP_DIR}/third-party/build/
+#cd ${TOP_DIR}/third-party/build/
+
+#cmake -DCMAKE_INSTALL_PREFIX=${TOP_DIR}/third-party/ -DUA_ENABLE_AMALGAMATION=true -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUA_ENABLE_FULL_NS0=ON ../open62541
+
+#make 
+#make install
+
+#mkdir -p ${TOP_DIR}/build
+#cd ${TOP_DIR}/build
+#cmake ..
+#make
+#make test
+
+
+
+
 
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 TOP_DIR=${SCRIPTS_DIR}/..
 
-mkdir ${TOP_DIR}/externals/build/
-cd ${TOP_DIR}/externals/build/
+export open62541_DIR=${TOP_DIR}/third-party/open62541/build/cmake
 
-cmake -DCMAKE_INSTALL_PREFIX=${TOP_DIR}/externals/ -DUA_ENABLE_AMALGAMATION=true -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUA_ENABLE_FULL_NS0=ON ../open62541
+#mkdir ${TOP_DIR}/third-party/open62541/build/
+#cd ${TOP_DIR}/third-party/open62541/build/
 
-make 
-make install
+#git checkout v0.3.0
+
+#cmake -DCMAKE_INSTALL_PREFIX=${TOP_DIR}/third-party/open62541/ \
+    #-DUA_ENABLE_AMALGAMATION=true \
+    #-DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    #-DUA_ENABLE_FULL_NS0=ON \
+    #..
+
+#make 
+#make install
 
 mkdir -p ${TOP_DIR}/build
 cd ${TOP_DIR}/build
